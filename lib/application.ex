022a3f41a90c57d8ff19a1823c95bf109172a6ba
esurat20 <esurat20@gmail.com>
2024-02-02 18:18:11 +0200

    WIP

---
+++ lib/application.ex
@@ -0,0 +1,16 @@
+defmodule ExBanking.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {ExBanking.User, []}
+    ]
+
+    opts = [
+      strategy: :one_for_one,
+      name: ExBanking.Supervisor
+    ]
+
+    Supervisor.start_link(children, opts)
+  end
+end
